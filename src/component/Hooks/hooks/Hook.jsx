de6232b37a41a4a8eb29179d4991b3a2567486ac
vipin yadav <vipinyadav31088@gmail.com>
2023-11-09 10:34:05 +0530

    first change

---
+++ src/component/Hooks/hooks/Hook.jsx
@@ -0,0 +1,16 @@
+import React  from 'react'
+// const bootstrap = require('bootstrap')
+const Hook = () => {
+  // const [ data , setData] = useState("");
+
+
+  return (
+    <div>
+      <h1>getting things with hooks component</h1>
+      <button className="btn btn-primary">hello world</button>
+      <h1 className='text-'></h1>
+    </div>
+  )
+}
+
+export default Hook;
